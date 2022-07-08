2133d8ffb311e8a6626b63dc4a2d39d92c5717d0
Ruth Rosales <ruth.rosales@breqwart.com>
2022-07-08 15:23:15 -0400

    Updated image names

--- ubuntu/main.tf
+++ ubuntu/main.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "ubuntu-2004-focal-amd64-qcow2" {
   count = var.enable_ubuntu_2004_amd64_qcow2 ? 1:0
-  name   = "TF Ubuntu 20.04"
+  name   = "Ubuntu 20.04"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/focal/current/focal-server-cloudimg-amd64.img"
   container_format = "bare"
   disk_format = "qcow2"
@@ -14,7 +14,7 @@ resource "openstack_images_image_v2" "ubuntu-2004-focal-amd64-qcow2" {
 }
 resource "openstack_images_image_v2" "ubuntu-1804-bionic-amd64-qcow2" {
   count = var.enable_ubuntu_1804_amd64_qcow2 ? 1:0
-  name   = "TF Ubuntu 18.04"
+  name   = "Ubuntu 18.04"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/xenial/current/xenial-server-cloudimg-amd64-disk1.img"
   container_format = "bare"
   disk_format = "qcow2"
