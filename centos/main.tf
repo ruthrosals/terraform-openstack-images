abb6614f0e36b756a9e3213b643eae49a60f1754
Ruth Rosales <ruth.rosales@breqwart.com>
2022-07-09 15:07:07 -0400

    updated main file

--- centos/main.tf
+++ centos/main.tf
@@ -15,7 +15,7 @@ resource "openstack_images_image_v2" "centos-7-x86_64-qcow2" {
 }
 resource "openstack_images_image_v2" "centos_8_x86_64_qcow2" {
   count            = var.enable_centos_8_x86_64_qcow2 ? 1 : 0
-  name             = var.centos8.name
+  name             = var.centos8_name
   image_source_url = "http://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
   container_format = var.image_container_format
   disk_format      = var.image_disk_format
